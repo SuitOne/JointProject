33157756bb813aa8530aaf33293ad9f90d023843
SuitOne <a.scesny@att.net>
2024-08-15 19:39:26 -0400

    Added boost test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-#include <stdio.h>
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
 
-int main() {
-	return 0;
+int main()
+{
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+        in(std::cin), in(), std::cout << (_1 * 3) << " ");
 }
